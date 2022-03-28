2e3a3ced44059a4f227a37506f4366b1496ebf9d
Dhuan Oliveira <dhuanco@gmail.com>
2022-03-28 20:04:59 +0200

    Setup command line structure

--- main.go
+++ main.go
@@ -1,63 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/dhuan/wikicmd/internal/config"
-	"github.com/dhuan/wikicmd/pkg/editor"
-	"github.com/dhuan/wikicmd/pkg/mw"
+	"github.com/dhuan/wikicmd/internal/cmd"
 )
 
 func main() {
-	config, err := config.Get()
-	if err != nil {
-		panic(err)
-	}
-
-	wikiConfig := mw.Config{
-		BaseAddress: config.Address,
-		Login:       config.User,
-		Password:    config.Password,
-	}
-
-	loginTokenSet, err := mw.GetLoginToken(&wikiConfig)
-	if err != nil {
-		panic("Failed to get login token.")
-	}
-	fmt.Println(fmt.Sprintf("Got Login Token Set\nCookie: %s\nToken:%s", loginTokenSet.Cookie, loginTokenSet.Token))
-
-	loginResult, err := mw.Login(&wikiConfig, loginTokenSet)
-	if err != nil {
-		panic("Failed to login.")
-	}
-	fmt.Println(fmt.Sprintf("Got Login Result Set\nCookie: %s", loginResult.Cookie))
-
-	csrfToken, err := mw.GetCsrfToken(&wikiConfig, loginTokenSet, loginResult)
-	if err != nil {
-		panic("Failed to get csrf token.")
-	}
-	fmt.Println(fmt.Sprintf("Got CSRF\nToken: %s", csrfToken.Token))
-
-	page, err := mw.GetPage(&wikiConfig, &mw.ApiCredentials{CsrfToken: csrfToken, LoginResult: loginResult}, "Some_page_title")
-	if err != nil {
-		fmt.Println(err)
-		panic("Failed to get page.")
-	}
-	fmt.Println(fmt.Sprintf("Page content: %s", page.Content))
-
-	newContent, err := editor.Edit(page.Content)
-	if err != nil {
-		panic("Failed to edit.")
-	}
-	fmt.Println(fmt.Sprintf("New content: %s", newContent))
-
-	_, err = mw.Edit(
-		&wikiConfig,
-		&mw.ApiCredentials{CsrfToken: csrfToken, LoginResult: loginResult},
-		"Some_page_title",
-		newContent,
-	)
-	if err != nil {
-		panic("Failed to edit.")
-	}
+	cmd.Execute()
 }
