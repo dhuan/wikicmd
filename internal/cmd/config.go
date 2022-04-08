249193058c0f4454d71301994c24d28cba84899e
Dhuan Oliveira <dhuanco@gmail.com>
2022-04-08 21:10:18 +0200

    Enhance "config" to create a new file if it doesn't exist

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -1,23 +1,89 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/dhuan/wikicmd/internal/config"
+	"github.com/dhuan/wikicmd/internal/input"
 	"github.com/dhuan/wikicmd/pkg/editor"
 	"github.com/spf13/cobra"
 )
+
+var defaultWiki = "https://wikipedia.org"
 
 var configCmd = &cobra.Command{
 	Use:   "config",
 	Short: "Edit your configuration file.",
 	Run: func(cmd *cobra.Command, filePaths []string) {
-		configFilePath, _, err := config.GetConfigFilePath()
+		configFilePath, configFileExists, err := config.GetConfigFilePath()
 		if err != nil {
 			panic(err)
+		}
+
+		if !configFileExists {
+			confirmed, err := newConfigWizard(configFilePath)
+			if err != nil {
+				panic(err)
+			}
+
+			if !confirmed {
+				fmt.Println("Aborted!")
+				os.Exit(1)
+			}
 		}
 
 		err = editor.EditFile(configFilePath)
 		if err != nil {
 			panic(err)
 		}
+
+		if configFileExists {
+			fmt.Println("Done!")
+		} else {
+			fmt.Println(fmt.Sprintf("Done!\n\nRun \"wikicmd config\" again whenever you want to edit that configuration file."))
+		}
 	},
+}
+
+func newConfigWizard(filePath string) (bool, error) {
+	fmt.Println("You don't seem to have any configuration file. Let's create one.")
+	fmt.Println("")
+	inputWikiAddress := input.TextPrompt(fmt.Sprintf("Wiki address: (%s) ", defaultWiki), defaultWiki)
+	inputLogin := input.TextPrompt("Login: ", "")
+	inputPassword := input.TextPrompt("Password: ", "")
+
+	fmt.Println(fmt.Sprintf("\nNext, a configuration file will be created for you and saved as %s\n", filePath))
+
+	inputConfirm, _ := input.ResponsePrompt[bool](
+		"Is this OK? (yes): ",
+		map[string]bool{"yes": true},
+		false,
+		true,
+	)
+
+	if !inputConfirm {
+		return false, nil
+	}
+
+	newConfig := config.ConfigRoot{
+		Config: []config.Config{
+			config.Config{
+				Id:       "my_wiki",
+				Address:  inputWikiAddress,
+				User:     inputLogin,
+				Password: inputPassword,
+			},
+		},
+	}
+
+	jsonEncoded, err := json.MarshalIndent(newConfig, "", "  ")
+	if err != nil {
+		return false, err
+	}
+
+	os.WriteFile(filePath, jsonEncoded, 0644)
+
+	return true, nil
 }
