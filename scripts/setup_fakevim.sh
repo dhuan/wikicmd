edcbda31bec0966a7a80a11ed87afce6724c5982
Dhuan Oliveira <dhuanco@gmail.com>
2022-07-27 11:42:47 +0200

    Make bin folder

--- scripts/setup_fakevim.sh
+++ scripts/setup_fakevim.sh
@@ -3,4 +3,5 @@ set -xe
 cd tests/fakevim
 make
 cd -
+mkdir -p ./tests/bin
 mv tests/fakevim/bin/fakevim tests/bin/.
--- scripts/setup_mock.sh
+++ scripts/setup_mock.sh
@@ -5,4 +5,5 @@ MOCK_DOWNLOAD_URL="https://github.com/dhuan/mock/releases/download/v0.0.2/mock_v
 TMP_PATH=$(mktemp -d)
 wget -P "$TMP_PATH" "$MOCK_DOWNLOAD_URL"
 tar xzvf "$TMP_PATH"/*.tar.gz -C "$TMP_PATH" 
+mkdir -p ./tests/bin
 mv "$TMP_PATH""/mock" ./tests/bin/.
