c3bfa69f1d030a0c31098c472937a2ed17fd36de
Dhuan Oliveira <dhuanco@gmail.com>
2023-09-08 22:54:39 +0200

    Bump mdbook

--- scripts/docs_build.sh
+++ scripts/docs_build.sh
@@ -1,6 +1,6 @@
 set -ex
 
-URL_MDBOOK="https://github.com/rust-lang/mdBook/releases/download/v0.4.18/mdbook-v0.4.18-x86_64-unknown-linux-gnu.tar.gz"
+URL_MDBOOK="https://github.com/rust-lang/mdBook/releases/download/v0.4.34/mdbook-v0.4.34-x86_64-unknown-linux-gnu.tar.gz"
 LATEST_VERSION=$(git describe --tags --abbrev=0 origin/master)
 
 if [[ -z "$WIKICMD_DOCS_BUILD_SAVE_AS" ]]
